d2c5f3cf56ad5fb9a7b05a3d685a0c88fe0d03a8
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-10-01 11:40:45 -0500

    Ensure usernames are unique/Intended Redirects/Error Messages & Handling

--- migration.sql
+++ migration.sql
@@ -3,9 +3,17 @@ USE adlister_db;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
 
+
+#TODO: Ensure usernames are unique. Change your database schema to enforce
+# that values in the username column of the user table are unique. Make sure
+# a new account cannot be created with an existing username.
+# -------------------------------------------------------------------------
+# It may be a good idea to add 'UNIQUE' requirement to 'email' as well,
+# as this is standard practice on most of the internet
+
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
+    username VARCHAR(240) NOT NULL UNIQUE,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -14,5 +14,11 @@ public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
         request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+
+        if (request.getSession().getAttribute("user") == null){
+            request.getSession().setAttribute("redirect", "/ads");
+            return;
+        }
+
     }
 }

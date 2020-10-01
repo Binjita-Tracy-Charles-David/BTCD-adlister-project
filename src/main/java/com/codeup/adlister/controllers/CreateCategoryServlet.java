a557805ad3c6d52900cd629586e22da1de73008d
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-10-01 15:10:19 -0500

    merge process

--- src/main/java/com/codeup/adlister/controllers/CreateCategoryServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateCategoryServlet.java
@@ -1,7 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
-import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Category;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -11,25 +11,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
-public class CreateAdServlet extends HttpServlet {
+@WebServlet(name = "controllers.CreateCategoryServlet", urlPatterns = "/categories/create")
+class CreateCategoryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
             return;
         }
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+        request.getRequestDispatcher("/WEB-INF/categories/create.jsp")
                 .forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User user = (User) request.getSession().getAttribute("user");
-        Ad ad = new Ad(
-                user.getId(),
-                request.getParameter("title"),
-                request.getParameter("description")
+        Category category = new Category(
+                request.getParameter("name")
         );
-        DaoFactory.getAdsDao().insert(ad);
-        response.sendRedirect("/ads");
+        DaoFactory.getCategoriesDao().insert(category);
+        response.sendRedirect("/categories");
     }
 }
---
+++ src/main/webapp/WEB-INF/categories/create.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tracyvelasquez
+  Date: 10/1/20
+  Time: 1:27 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="/WEB-INF/partials/head.jsp">
+        <jsp:param name="title" value="Create a new Category" />
+    </jsp:include>
+</head>
+<body>
+<div class="container">
+    <h1>Create a new Category</h1>
+    <form action="/categories/create" method="post">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input id="title" name="title" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea id="description" name="description" class="form-control" type="text"></textarea>
+        </div>
+        <input type="submit" class="btn btn-block btn-primary">
+    </form>
+</div>
+</body>
+</html>
